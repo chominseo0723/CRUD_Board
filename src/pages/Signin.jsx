6b6c7810c65c5be7b47a64bf4742d5a183118d19
chominseo <singer100@yu.ac.kr>
2026-01-26 10:45:17 +0900

    feature : 로그인 UI

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -4,8 +4,24 @@ import React from 'react'
 const Signin = () => {
   return (
     <>
-    <div>
-        <span className='font-pretendard'>로그인</span>
+    <div className='flex flex-col min-h-screen font-pretendard items-center justify-center gap-10'>
+        <span className='text-3xl font-semibold'>로그인</span>
+
+        <form className='flex flex-col gap-5'>
+            <div className='flex flex-col gap-3'>
+                <span className='text-xl font-semibold'>아이디</span>
+                <input className='border rounded-[10px] pl-3 py-2 w-90 border-[#A3A3A3]' placeholder ="아이디를 입력하세요"/>
+            </div>
+
+            <div className='flex flex-col gap-3'>
+                <span className='text-xl font-semibold'>비밀번호</span>
+                <input className='border rounded-[10px] pl-3 py-2 w-90 border-[#A3A3A3]' placeholder="비밀번호를 입력하세요" type="password"/>
+            </div>
+
+            <button className='border border-[#87C1FF] 
+            bg-[#87C1FF] py-2 rounded-[10px] text-white
+            ' type="submit">로그인</button>
+        </form>
     </div>
 
     </>
