6ee9a7da812de1922213a5314f2674fcd9b2e257
chominseo <singer100@yu.ac.kr>
2026-01-28 10:41:29 +0900

    fix : 프론트 포트 번호 허용

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 const authRouter = require('./routes/auth.routes');
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5174',
   credentials: true,
 }));
 
